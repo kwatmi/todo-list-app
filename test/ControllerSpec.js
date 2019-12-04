6fd42a118b32084de7699bd5a6a7eadfea45d7e2
Kwatmi Haruna Paul <kwatmiharunapaul@gmail.com>
2019-12-04 23:27:53 +0100

    add new todo clean up

--- test/ControllerSpec.js
+++ test/ControllerSpec.js
@@ -224,8 +224,7 @@ describe('controller', function () {
 			subject.setView("");
 		it('should add a new todo to the model', function () {
 			// TODO: write test
-			setUpModel([]);
-			subject.setView("");
+		
 			 view.trigger("newTodo", "a new todo");
 			 expect(model.create).toHaveBeenCalledWith(
 				 "a new todo",
