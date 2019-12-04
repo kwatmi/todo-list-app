76e48222d0785eee44c60d5f949cbcd21044d52a
Kwatmi Haruna Paul <kwatmiharunapaul@gmail.com>
2019-12-04 23:25:55 +0100

    should add new todo

--- test/ControllerSpec.js
+++ test/ControllerSpec.js
@@ -221,6 +221,13 @@ describe('controller', function () {
 	describe('new todo', function () {
 		it('should add a new todo to the model', function () {
 			// TODO: write test
+			setUpModel([]);
+			subject.setView("");
+			 view.trigger("newTodo", "a new todo");
+			 expect(model.create).toHaveBeenCalledWith(
+				 "a new todo",
+
+			 );
 		});
 
 		it('should add a new todo to the view', function () {
