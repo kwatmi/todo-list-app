30147f59180631e194510508877275e9c27cca4c
Kwatmi Haruna Paul <kwatmiharunapaul@gmail.com>
2019-12-10 22:45:24 +0100

    should show completed entries

--- test/ControllerSpec.js
+++ test/ControllerSpec.js
@@ -103,6 +103,17 @@ describe('controller', function () {
 
 		it('should show completed entries', function () {
 			// TODO: write test
+			var todo = { title: "my todo", completed: true };
+			setUpModel([todo]);
+
+			subject.setView("#/completed");
+
+			expect(model.read).toHaveBeenCalledWith(
+				{ completed: true },
+				jasmine.any(Function)
+			);
+
+			expect(view.render).toHaveBeenCalledWith("showEntries", [todo]);
 		});
 	});
 
